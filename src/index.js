9d15d76851602a8ccc7542a04b39ef2876ea1d6f
Brian Mahecha <mahebd@mail.broward.edu>
2018-03-05 08:32:45 -0500

    adding readme

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ class App extends React.Component {
             <div className="main">
                 <img className="image" src={require('./assets/mahechabjj.png')} alt="mahechabjj" />
                 <div className="appstore">
-                    <a href="https://itunes.apple.com/us/app/mahecha-bjj/id1330507366?mt=8" target="_blank"><img className="icon" src={require('./assets/applestore.png')} alt="apple store" /></a>
-                    <a href="https://play.google.com/store/apps/details?id=com.mahechabjj.mahecha_bjj" target="_blank"><img className="icon" src={require('./assets/googlestore.png')} alt="google store" /></a>
+                    <a href="https://itunes.apple.com/us/app/mahecha-bjj/id1330507366?mt=8" target="_blank" rel="noopener noreferrer"><img className="icon" src={require('./assets/applestore.png')} alt="apple store" /></a>
+                    <a href="https://play.google.com/store/apps/details?id=com.mahechabjj.mahecha_bjj" target="_blank" rel="noopener noreferrer"><img className="icon" src={require('./assets/googlestore.png')} alt="google store" /></a>
                 </div>
             </div>
         );
