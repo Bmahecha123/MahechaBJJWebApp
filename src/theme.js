dd94542868d838e2ca7c17cc5ea73c018df34c3c
Brian Mahecha <mahebd@mail.broward.edu>
2018-04-08 14:06:18 -0400

    added base styles

---
+++ src/theme.js
@@ -0,0 +1,29 @@
+const palette = {
+    white: "#f9fafd",
+    black: "#070d18",
+    blue: "#355dae",
+    red: "#7c2529"
+};
+
+const colors = {
+    primaryTextColor: palette.black,
+    backgroundColor: palette.white,
+    buttonBackgroundColor: palette.blue,
+    boxShadow: "0 2px 14px 4px rgba(53, 64, 134, 0.60);"
+};
+
+const spacing = {
+    small: "1rem", //16px
+    medium: "2rem", //32px
+    large: "4rem", //64px
+    xlarge: "6rem" //94px
+};
+
+const fontSizing = {
+    small: "1rem",
+    medium: "2rem",
+    large: "3rem",
+    xlarge: "4rem"
+};
+
+export { colors, spacing, fontSizing };
